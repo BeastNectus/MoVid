a00c6f7ebbe69c089b16e26478a2ca6db37c5574
John Mamanao <kingfrost1143@gmail.com>
2024-03-28 16:48:03 +0800

    changed the totalpages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
 
   fetchAllMovies() {
     this.isLoading = true;
-    const totalPages = 2000;
+    const totalPages = 1000;
     let fetchedMovies: any[] = [];
     for (let page = 1; page <= totalPages; page++) {
       this.subscription = this.movieService.getNewMovies(page)
